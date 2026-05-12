dab48ff357dd2cc9c4bb9dd0689f26483ee47739
Banana-4 <stig34768@gmail.com>
2026-05-12 14:37:49 +0000

    add main menu state

--- modules/game.py
+++ modules/game.py
@@ -1,34 +1,44 @@
 import pygame
-from state_interface import State
-from main_menu import MainMenu
+
+from modules.custom_events import QUIT_GAME
+from modules.main_menu import MainMenu
+from modules.state_interface import State
+
 
 class Game:
-    def __init__(self, res: tuple[int, int] = (640, 400)) -> None:
+    def __init__(self, win_size: tuple[int, int] = (640, 400)) -> None:
+        if win_size[0] > 0 and win_size[1] > 0:
+            self.win_size = win_size
+        else:
+            self.win_size = (640, 400)
+        pygame.init()
         self.caption: str = "Space Invaders"
-        self.win: pygame.Surface  = pygame.display.set_mode(res)
+        self.win: pygame.Surface = pygame.display.set_mode(self.win_size)
         pygame.display.set_caption(self.caption)
         self.clock: pygame.time.Clock = pygame.time.Clock()
         self.run = True
         self.fps = 60
-        self.state: State  = MainMenu()
+        self.state: State = MainMenu(self.win_size)
 
-    def main(self) -> None: # game loop
+    def main(self) -> None:  # game loop
         dt = 0
-        while self.run:
+        while True:
             dt = self.clock.tick(self.fps)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.run = False
-                    break
-            self.handle_input()
-            self.update(dt)
-            self.draw()
+            if self.run:
+                self.handle_input()
+                self.update(dt)
+                self.draw()
+            else:
+                break
 
     def update(self, dt: float) -> None:
         self.state.update(dt)
 
     def handle_input(self) -> None:
         self.state.handle_input()
+        for event in pygame.event.get():
+            if event.type == QUIT_GAME:
+                self.run = False
 
     def draw(self) -> None:
         self.win.fill("black")
